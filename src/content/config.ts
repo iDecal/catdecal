39b2a23277ae0debfb030813d548142c71a9c41e
iDecal <105539236+iDecal@users.noreply.github.com>
2026-04-19 23:42:28 +0700

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,17 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const blogCollection = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
     excerpt: z.string(),
     category: z.string(),
-    date: z.string(), // Hoặc z.date() nếu bạn dùng định dạng chuẩn YYYY-MM-DD
+    date: z.string(),
     image: z.string(),
   }),
 });
 
+// Tên key ở đây (blog) PHẢI trùng với tên thư mục (src/content/blog)
 export const collections = {
-  'blog': blog,
+  'blog': blogCollection,
 };
