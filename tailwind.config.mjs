3306c07e3e2d594bcd2f0d9c3bef5fccb8e70b9b
iDecal <105539236+iDecal@users.noreply.github.com>
2026-04-19 14:20:20 +0700

    Create tailwind.config.mjs

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#ffdf3c',   // Vàng
+				secondary: '#0b3954', // Đen
+				accent: '#BAE6FD',    // Xanh da nhạt
+				background: '#FFFFFF' // Nền trắng
+			},
+			fontFamily: {
+				heading: ['"Be Vietnam Pro"', 'sans-serif'],
+				body: ['Inter', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+}
