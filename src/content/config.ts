5c50bc1d3d0e96cd0546f71365d67d1997631001
iDecal <105539236+iDecal@users.noreply.github.com>
2026-04-19 23:39:37 +0700

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,9 +6,11 @@ const blog = defineCollection({
     title: z.string(),
     excerpt: z.string(),
     category: z.string(),
-    date: z.string(),
+    date: z.string(), // Hoặc z.date() nếu bạn dùng định dạng chuẩn YYYY-MM-DD
     image: z.string(),
   }),
 });
 
-export const collections = { blog };
+export const collections = {
+  'blog': blog,
+};
