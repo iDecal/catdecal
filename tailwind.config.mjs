1365abc56d4a5d4a0af5f70c58f2030811f4b872
iDecal <105539236+iDecal@users.noreply.github.com>
2026-04-19 15:56:09 +0700

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,7 @@ export default {
 		extend: {
 			colors: {
 				// Đổi Đen thành Primary (Chủ đạo)
-				primary: '#EB4C4C',   
+				primary: '#ffffff',   
 				
 				// Đổi Vàng chanh thành Secondary (Màu nhấn cho button, link)
 				secondary: '#DB1A1A', 
