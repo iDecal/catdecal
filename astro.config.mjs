7dcb220bb292fa2ce12615ff6c0a99011d9c1319
iDecal <105539236+iDecal@users.noreply.github.com>
2026-04-27 15:53:17 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
+import keystatic from '@keystatic/astro';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://idecal.vn', // Thay bằng domain của bạn
+  site: 'https://idecal.vn',
+   output: 'hybrid', 
   integrations: [tailwind(), sitemap()],
   // Tích hợp Tailwind CSS
   integrations: [tailwind()],
