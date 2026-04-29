a80fa83d435818f1605f3606af88d779c6fa466d
iDecal <105539236+iDecal@users.noreply.github.com>
2026-04-30 00:24:58 +0700

    Create contentful.ts

---
+++ src/lib/contentful.ts
@@ -0,0 +1,8 @@
+// src/lib/contentful.ts
+import contentful from "contentful";
+
+export const contentfulClient = contentful.createClient({
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.CONTENTFUL_ACCESS_TOKEN,
+  host: "cdn.contentful.com",
+});
